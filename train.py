a6953b40f152e13e0ddccea827fd9923e9425b09
Julian Engelhardt <jengelhardt211@gmail.com>
2018-10-06 18:03:33 +0200

    Broaden to 2000 outputs on first dense layer
    Scores ~0.13 on Kaggle

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 from tensorflow import keras
 from scipy.stats import skew
@@ -40,15 +41,17 @@ print(X_test.shape)
 train_y = train.SalePrice
 
 model = keras.Sequential()
-model.add(keras.layers.Dense(256, input_dim = X_train.shape[1]))
+model.add(keras.layers.Dense(2000, input_dim = X_train.shape[1]))
 model.add(keras.layers.Dropout(0.1))
-model.add(keras.layers.BatchNormalization())
+model.add(keras.layers.Dense(1000))
+model.add(keras.layers.Dense(500))
+model.add(keras.layers.Dense(256))
 model.add(keras.layers.Dense(128))
 model.add(keras.layers.Dense(56))
-model.add(keras.layers.BatchNormalization())
 model.add(keras.layers.Dense(28))
 model.add(keras.layers.Dense(14))
 model.add(keras.layers.Dense(8))
+model.add(keras.layers.BatchNormalization())
 model.add(keras.layers.Dense(1, kernel_regularizer=keras.regularizers.l1(0.01)))
 model.compile(loss = "mse", optimizer=keras.optimizers.Adam())
 
@@ -58,15 +61,33 @@ partial_x_train = X_train[200:]
 y_val = train_y[:200]
 partial_y_train = train_y[200:]
 
-hist = model.fit(partial_x_train, partial_y_train, epochs=100, batch_size=20, validation_data = (x_val, y_val))
+run_epochs = int(os.environ['EPOCHS'])
+
+tensorBoardCallback = keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=10, write_graph=True, write_images=True)
+
+checkpoint_epochs = int(os.environ['CHECKPOINT_EPOCHS'])
+checkpoint_path = "checkpoints/cp-{epoch:04d}.ckpt"
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+    checkpoint_path, verbose=1, save_weights_only=True,
+    # Save weights, every 5-epochs.
+    period=checkpoint_epochs)
+
+print('Training with {} epochs, saving weights all {} epochs.'.format(run_epochs, checkpoint_epochs))
+
+if 'CHECKPOINT_FILE' in os.environ and os.environ['CHECKPOINT_FILE']:
+    reload_checkpoint = os.environ['CHECKPOINT_FILE']
+    print('Loading stored weights from {}'.format(reload_checkpoint))
+    model.load_weights(reload_checkpoint)
+
+hist = model.fit(partial_x_train, partial_y_train, epochs=run_epochs, batch_size=20, validation_data = (x_val, y_val), callbacks=[tensorBoardCallback, cp_callback])
 print(hist.history.keys())
 print("train loss min: {}, val loss min: {}, train loss mean: {}, val loss mean: {}".format(np.amin(hist.history['loss']), np.amin(hist.history['val_loss']), np.mean(hist.history['loss']), np.mean(hist.history['val_loss'])))
 
-plt.plot(hist.history['loss'])
-plt.plot(hist.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
+# plt.plot(hist.history['loss'])
+# plt.plot(hist.history['val_loss'])
+# plt.title('model loss')
+# plt.ylabel('loss')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+# plt.show()
 model.save('model_v1.h5');
