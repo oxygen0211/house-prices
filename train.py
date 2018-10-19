2e0c41e62fa20e02650ab972938c78445135752b
Julian Engelhardt <jengelhardt211@gmail.com>
2018-10-19 21:09:01 +0200

    Add preprocessing for non linear and categorial data

--- train.py
+++ train.py
@@ -7,10 +7,219 @@ import os
 from tensorflow import keras
 from scipy.stats import skew
 
+def qual_conv(q):
+    if q=='Po':
+        return 0
+    if q=='Fa':
+        return 1
+    if q=='TA':
+        return 2
+    if q=='Gd':
+        return 3
+    if q=='Ex':
+        return 4
+    return 5
+
+def slope_conv(s):
+    if s=='Gtl':
+        return 0
+    if s=='Mod':
+        return 1
+    if s=='Sev':
+        return 2
+    return 3
+
+def garage_finish_conv(f):
+    if f=='Unf':
+        return 0
+    if f=='RFn':
+        return 1
+    if f=='Fin':
+        return 2
+    return 3
+
+def bsmt_exposure_conv(e):
+    if e=='No':
+        return 0
+    if e=='Mn':
+        return 1
+    if e=='Av':
+        return 2
+    if e=='Gd':
+        return 3
+    return 4
+
+def bsmt_fin_type_conf(t):
+    if t=='Unf':
+        return 0
+    if t=='LwQ':
+        return 1
+    if t=='Rec':
+        return 2
+    if t=='BLQ':
+        return 3
+    if t=='ALQ':
+        return 4
+    if t=='GLQ':
+        return 5
+    return 6
+
+def functional_conv(f):
+    if f=='Typ':
+        return 0
+    if f=='Min1':
+        return 1
+    if f=='Min2':
+        return 2
+    if f=='Mod':
+        return 3
+    if f=='Maj1':
+        return 4
+    if f=='Maj2':
+        return 5
+    if f=='Sev':
+        return 6
+    if f=='Sal':
+        return 0
+
+def house_style_conv(s):
+    if s=='1Story':
+        return 0
+    if s=='1.5Fin':
+        return 1
+    if s=='1.5Unf':
+        return 2
+    if s=='2Story':
+        return 3
+    if s=='2.5Fin':
+        return 4
+    if s=='2.5Unf':
+        return 5
+    if s=='SFoyer':
+        return 6
+    if s=='SLvl':
+        return 7
+    return 8
+
+def foundation_conv(f):
+    if f=='BrkTil':
+        return 0
+    if f=='CBlock':
+        return 1
+    if f=='PConc':
+        return 2
+    if f=='Slab':
+        return 3
+    if f=='Stone':
+        return 4
+    if f=='Wood':
+        return 5
+    return 6
+
+def mas_ver_type_conv(t):
+    if t=='BrkCmn':
+        return 0
+    if t=='BrkFace':
+        return 1
+    if t=='CBlock':
+        return 2
+    if t=='None':
+        return 3
+    if t=='Stone':
+        return 4
+    return 5
+
+def addlogs(res, ls):
+    m = res.shape[1]
+    for l in ls:
+        res = res.assign(newcol=pd.Series(np.log(1.01+res[l])).values)
+        res.columns.values[m] = l + '_log'
+        m += 1
+    return res
+
+def addSquared(res, ls):
+    m = res.shape[1]
+    for l in ls:
+        res = res.assign(newcol=pd.Series(res[l]*res[l]).values)
+        res.columns.values[m] = l + '_sq'
+        m += 1
+    return res
+
+
 train = pd.read_csv('./data/train.csv')
 test = pd.read_csv('./data/test.csv')
 
-print(train.describe())
+train.drop(train[(train['GrLivArea']>4000)].index, inplace=True)
+train.drop(train[(train['OverallQual']<5) & (train['SalePrice']>200000)].index, inplace=True)
+train.reset_index(drop=True, inplace=True)
+
+# plt.scatter(train['YearRemodAdd'], train['SalePrice'])
+# plt.xlabel('YearRemodAdd')
+# plt.ylabel('SalePrice')
+# plt.show()
+
+cols_to_log=['LotFrontage', 'LotArea','MasVnrArea','BsmtFinSF1','BsmtFinSF2','BsmtUnfSF',
+                 'TotalBsmtSF','1stFlrSF','2ndFlrSF','LowQualFinSF','GrLivArea',
+                 'BsmtFullBath','BsmtHalfBath','FullBath','HalfBath','BedroomAbvGr','KitchenAbvGr',
+                 'TotRmsAbvGrd','Fireplaces','GarageCars','GarageArea','WoodDeckSF','OpenPorchSF',
+                 'EnclosedPorch','3SsnPorch','ScreenPorch','PoolArea','MiscVal','YearRemodAdd']
+
+cols_to_square=['YearRemodAdd', 'LotFrontage',
+              'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 'GrLivArea',
+              'GarageCars', 'GarageArea',
+              'OverallQual','ExterQual','BsmtQual','GarageQual','FireplaceQu','KitchenQual']
+
+train['ExterQual'] = train['ExterQual'].apply(qual_conv)
+train['ExterCond'] = train['ExterCond'].apply(qual_conv)
+train['BsmtQual'] = train['BsmtQual'].apply(qual_conv)
+train['BsmtCond'] = train['BsmtCond'].apply(qual_conv)
+train['KitchenQual'] = train['KitchenQual'].apply(qual_conv)
+train['FireplaceQu'] = train['FireplaceQu'].apply(qual_conv)
+train['GarageQual'] = train['GarageQual'].apply(qual_conv)
+train['PoolQC'] = train['PoolQC'].apply(qual_conv)
+train['HeatingQC'] = train['HeatingQC'].apply(qual_conv)
+
+train['LandSlope'] = train['LandSlope'].apply(slope_conv)
+
+train['CentralAir'] = train['CentralAir'].apply(lambda x: 1 if x=='Y' else 0)
+train['Street'] = train['Street'].apply(lambda x: 1 if x=='Pave' else 0)
+train['PavedDrive'] = train['PavedDrive'].apply(lambda x: 1 if x=='Y' else 0)
+train['GarageFinish'] = train['GarageFinish'].apply(garage_finish_conv)
+train['BsmtExposure'] = train['BsmtExposure'].apply(bsmt_exposure_conv)
+train['BsmtFinType1'] = train['BsmtFinType1'].apply(bsmt_fin_type_conf)
+train['BsmtFinType2'] = train['BsmtFinType2'].apply(bsmt_fin_type_conf)
+train['HouseStyle'] = train['HouseStyle'].apply(house_style_conv)
+train['Foundation'] = train['Foundation'].apply(foundation_conv)
+train['MasVnrType'] = train['MasVnrType'].apply(mas_ver_type_conv)
+
+train = addlogs(train, cols_to_log)
+train = addSquared(train, cols_to_square)
+
+test['ExterQual'] = test['ExterQual'].apply(qual_conv)
+test['ExterCond'] = test['ExterCond'].apply(qual_conv)
+test['BsmtQual'] = test['BsmtQual'].apply(qual_conv)
+test['BsmtCond'] = test['BsmtCond'].apply(qual_conv)
+test['KitchenQual'] = test['KitchenQual'].apply(qual_conv)
+test['FireplaceQu'] = test['FireplaceQu'].apply(qual_conv)
+test['GarageQual'] = test['GarageQual'].apply(qual_conv)
+test['PoolQC'] = test['PoolQC'].apply(qual_conv)
+test['HeatingQC'] = test['HeatingQC'].apply(qual_conv)
+
+test['LandSlope'] = test['LandSlope'].apply(slope_conv)
+
+test['CentralAir'] = test['CentralAir'].apply(lambda x: 1 if x=='Y' else 0)
+test['Street'] = test['Street'].apply(lambda x: 1 if x=='Pave' else 0)
+test['PavedDrive'] = test['PavedDrive'].apply(lambda x: 1 if x=='Y' else 0)
+test['GarageFinish'] = test['GarageFinish'].apply(garage_finish_conv)
+test['BsmtExposure'] = test['BsmtExposure'].apply(bsmt_exposure_conv)
+test['BsmtFinType1'] = test['BsmtFinType1'].apply(bsmt_fin_type_conf)
+test['BsmtFinType2'] = test['BsmtFinType2'].apply(bsmt_fin_type_conf)
+test['HouseStyle'] = test['HouseStyle'].apply(house_style_conv)
+test['Foundation'] = test['Foundation'].apply(foundation_conv)
+test['MasVnrType'] = test['MasVnrType'].apply(mas_ver_type_conv)
+
+test = addlogs(test, cols_to_log)
+test = addSquared(test, cols_to_square)
 
 all_data = pd.concat((train.loc[:,'MSSubClass':'SaleCondition'],
                       test.loc[:,'MSSubClass':'SaleCondition']))
@@ -63,7 +272,7 @@ partial_y_train = train_y[200:]
 
 run_epochs = int(os.environ['EPOCHS'])
 
-tensorBoardCallback = keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=10, write_graph=True, write_images=True)
+tensorBoardCallback = keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=100, write_graph=True, write_images=True)
 
 checkpoint_epochs = int(os.environ['CHECKPOINT_EPOCHS'])
 checkpoint_path = "checkpoints/cp-{epoch:04d}.ckpt"
